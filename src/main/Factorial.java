61520a912b0cc7a583ac3d249bd522b56003d616
rgoncar723 <rgoncar723@g.edcuaand.es>
2025-12-10 08:59:15 +0100

    Segundo commit, se añadio la funcion de pedirNumero, optimizacion del codigo

--- src/main/Factorial.java
+++ src/main/Factorial.java
@@ -1,20 +1,25 @@
 package main;
+
 import java.util.Scanner;
+
 public class Factorial {
+	private static Scanner keyboard = new Scanner(System.in);
 
-		public static void main(String[] args) {
-	        Scanner keyboard = new Scanner(System.in);
+	private static int pedirNumero() {
+		System.out.print("Introduce un número entero mayor o igual que 0: ");
+		return keyboard.nextInt();
+	}
 
-	        System.out.print("Introduce un número entero mayor o igual que 0: ");
-	        int number = keyboard.nextInt();
+	public static void main(String[] args) {
 
-	        if (number < 0) {
-	            System.out.println("ERROR: el número debe ser mayor o igual que 0.");
-	        } else {
-	            System.out.println("Has introducido el número: " + number);
-	        }
+		int number = pedirNumero();
 
-	        keyboard.close();
+		if (number < 0) {
+			System.out.println("ERROR: el número debe ser mayor o igual que 0.");
+		} else {
+			System.out.println("Has introducido el número: " + number);
 		}
+
+	}
 
 }
